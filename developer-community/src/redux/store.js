b279e3eb4b249543182db55e147564fd827b8b08
xiaomeng831 <bradyxu831@gmail.com>
2021-12-16 17:15:16 -0500

    set redux devtools

--- developer-community/src/redux/store.js
+++ developer-community/src/redux/store.js
@@ -1,5 +1,6 @@
-import {createStore} from 'redux'
+import {createStore, applyMiddleware} from 'redux'
+import {composeWithDevTools} from 'redux-devtools-extension'
 import reducer from './reducers'
 
-const store = createStore(reducer)
+const store = createStore(reducer, composeWithDevTools(applyMiddleware()))
 export default store
